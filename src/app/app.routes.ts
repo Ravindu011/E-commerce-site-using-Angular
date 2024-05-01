b52e0de85b541051cadf9befb316080f29745c1e
Ravindu C Rathnayake <ravinduc2001@gmail.com>
2024-05-01 18:13:09 +0530

    Admin Product Ui created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './admin/login/login.component';
+import { LayoutComponent } from './admin/layout/layout.component';
+import path from 'path';
+import { ProductsComponent } from './admin/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'products',
+                component:ProductsComponent
+            }
+            
+
+        ]
+    }
+];
